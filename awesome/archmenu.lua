779716b4c2e42e71fc39ac31a2b8c8ec4340df73
Spigerz <josephallbeck@gmail.com>
2026-03-16 02:03:02 -0400

    Add files via upload

---
+++ awesome/archmenu.lua
@@ -0,0 +1,68 @@
+ local menu98edb85b00d9527ad5acebe451b3fae6 = {
+     {"Files", "nautilus --new-window ", "/usr/share/icons/hicolor/scalable/apps/org.gnome.Nautilus.svg" },
+     {"HP Device Manager", "hp-toolbox", "///usr/share/hplip/data/images/128x128/hp_logo.png" },
+     {"Micro", "xterm -e micro ", "/usr/share/icons/hicolor/scalable/apps/micro.svg" },
+     {"Vim", "xterm -e vim ", "/usr/share/icons/hicolor/48x48/apps/gvim.png" },
+     {"Winetricks", "winetricks --gui", "/usr/share/icons/hicolor/scalable/apps/winetricks.svg" },
+ }
+
+ local menu251bd8143891238ecedc306508e29017 = {
+     {"Steam", "/usr/bin/steam ", "/usr/share/icons/hicolor/16x16/apps/steam.png" },
+ }
+
+ local menuc8205c7636e728d448c2774e6a4a944b = {
+     {"Avahi SSH Server Browser", "/usr/bin/bssh", "/usr/share/icons/AdwaitaLegacy/16x16/legacy/network-wired.png" },
+     {"Avahi VNC Server Browser", "/usr/bin/bvnc", "/usr/share/icons/AdwaitaLegacy/16x16/legacy/network-wired.png" },
+     {"Firefox", "/usr/lib/firefox/firefox ", "/usr/share/icons/hicolor/16x16/apps/firefox.png" },
+     {"Steam", "/usr/bin/steam ", "/usr/share/icons/hicolor/16x16/apps/steam.png" },
+ }
+
+ local menue6f43c40ab1c07cd29e4e83e4ef6bf85 = {
+     {"Code - OSS", "code-oss ", "/usr/share/icons/hicolor/scalable/apps/com.visualstudio.code.oss.svg" },
+     {"Meld", "meld ", "/usr/share/icons/hicolor/scalable/apps/org.gnome.Meld.svg" },
+     {"Micro", "xterm -e micro ", "/usr/share/icons/hicolor/scalable/apps/micro.svg" },
+ }
+
+ local menuf4f70727dc34561dfde1a3c529b6205c = {
+     {"Advanced Network Configuration", "nm-connection-editor", "/usr/share/icons/AdwaitaLegacy/16x16/legacy/preferences-system-network.png" },
+     {"Bluetooth Manager", "blueman-manager", "/usr/share/icons/hicolor/16x16/apps/blueman.png" },
+     {"Customize Look and Feel", "lxappearance", "/usr/share/icons/AdwaitaLegacy/16x16/legacy/preferences-desktop-theme.png" },
+     {"LightDM GTK Greeter settings", "lightdm-gtk-greeter-settings-pkexec", "/usr/share/icons/hicolor/16x16/apps/lightdm-gtk-greeter-settings.png" },
+     {"Manage Printing", "xdg-open http://localhost:631/", "/usr/share/icons/hicolor/16x16/apps/cups.png" },
+     {"NVIDIA X Server Settings", "/usr/bin/nvidia-settings", "/usr/share/pixmaps/nvidia-settings.png" },
+     {"Print Settings", "system-config-printer", "/usr/share/icons/AdwaitaLegacy/16x16/devices/printer.png" },
+     {"Volume Control", "pavucontrol", "/usr/share/icons/hicolor/scalable/apps/org.pulseaudio.pavucontrol.svg" },
+ }
+
+ local menu52dd1c847264a75f400961bfb4d1c849 = {
+     {"Qt V4L2 test Utility", "qv4l2", "/usr/share/icons/hicolor/16x16/apps/qv4l2.png" },
+     {"Qt V4L2 video capture utility", "qvidcap", "/usr/share/icons/hicolor/16x16/apps/qvidcap.png" },
+     {"Volume Control", "pavucontrol", "/usr/share/icons/hicolor/scalable/apps/org.pulseaudio.pavucontrol.svg" },
+ }
+
+ local menuee69799670a33f75d45c57d1d1cd0ab3 = {
+     {"Alacritty", "alacritty", "/usr/share/pixmaps/Alacritty.svg" },
+     {"Avahi Zeroconf Browser", "/usr/bin/avahi-discover", "/usr/share/icons/AdwaitaLegacy/16x16/legacy/network-wired.png" },
+     {"btop++", "xterm -e btop", "/usr/share/icons/hicolor/48x48/apps/btop.png" },
+     {"CachyOS Hello", "/usr/bin/cachyos-hello", "/usr/share/icons/hicolor/scalable/apps/org.cachyos.hello.svg" },
+     {"CachyOS Kernel Manager", "cachyos-kernel-manager", "/usr/share/icons/hicolor/16x16/apps/org.cachyos.KernelManager.png" },
+     {"CachyOS Package Installer", "/usr/bin/cachyos-pi", "/usr/share/icons/hicolor/scalable/apps/cachyos-pi.png" },
+     {"Octopi", "/usr/bin/octopi", "/usr/share/icons/hicolor/48x48/apps/octopi.png" },
+     {"Octopi CacheCleaner", "/usr/bin/octopi-cachecleaner", "/usr/share/icons/hicolor/48x48/apps/octopi.png" },
+     {"Octopi Notifier", "/usr/bin/octopi-notifier", "/usr/share/icons/hicolor/48x48/apps/octopi.png" },
+     {"Octopi Repository Editor", "/usr/bin/octopi-repoeditor", "/usr/share/icons/hicolor/48x48/apps/octopi.png" },
+     {"SchedExt GUI Manager", "scx-manager", "/usr/share/icons/hicolor/scalable/apps/org.cachyos.scx-manager.png" },
+     {"UXTerm", "uxterm", "/usr/share/pixmaps/xterm-color_48x48.xpm" },
+     {"XTerm", "xterm", "/usr/share/pixmaps/xterm-color_48x48.xpm" },
+ }
+
+xdgmenu = {
+    {"Accessories", menu98edb85b00d9527ad5acebe451b3fae6, "/usr/share/icons/AdwaitaLegacy/16x16/legacy/applications-accessories.png"},
+    {"Games", menu251bd8143891238ecedc306508e29017, "/usr/share/icons/AdwaitaLegacy/16x16/legacy/applications-games.png"},
+    {"Internet", menuc8205c7636e728d448c2774e6a4a944b, "/usr/share/icons/AdwaitaLegacy/16x16/legacy/applications-internet.png"},
+    {"Programming", menue6f43c40ab1c07cd29e4e83e4ef6bf85, "/usr/share/icons/AdwaitaLegacy/16x16/legacy/applications-development.png"},
+    {"Settings", menuf4f70727dc34561dfde1a3c529b6205c, "/usr/share/icons/AdwaitaLegacy/16x16/legacy/preferences-desktop.png"},
+    {"Sound & Video", menu52dd1c847264a75f400961bfb4d1c849, "/usr/share/icons/AdwaitaLegacy/16x16/legacy/applications-multimedia.png"},
+    {"System Tools", menuee69799670a33f75d45c57d1d1cd0ab3, "/usr/share/icons/AdwaitaLegacy/16x16/legacy/applications-system.png"},
+}
+
